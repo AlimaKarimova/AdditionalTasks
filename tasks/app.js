00328ad56a24d641f3d2b9d7ce15357c611d2b67
AlimaKarimova <way.s@inbox.ru>
2022-04-29 22:51:28 +0400

    .

--- tasks/app.js
+++ tasks/app.js
@@ -7,12 +7,16 @@
 // // 
 // multiplyNumbers()
 
+
+
 //2. Написать функцию, которой передаем, имя, фамилия и возраст, и получаем строку "Привет Иван Петров с возрастом 17 лет" (только здесь данные, которые были переданы в функцию)
 
 // let firstName = prompt('Adinizi daxil edin');
 // let lastName = prompt('Soyadinizi daxil edin');
 // let Age = prompt('Yasinizi daxil edin');
 // alert(`Salam ${firstName} ${lastName} (${Age} yas)`)
+
+
 
 //3. Написать функцию, которая принимает пол человека ('M','F') в виде строки, результат функции возвращает строку "Ваш пол мужской" (или женский) или же "Ваш пол не опеределен"
 
@@ -32,10 +36,13 @@
 //     }
 // }
 
+
+
 //4. Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
 // let eded = prompt('Eded daxil edin')
+// let gunler=['mon', 'tue', 'wen', 'thu', 'fri','sut', 'sun' ]
 
-//5. function gunler(eded) {
+// function gunler(eded) {
 //     if (eded >= '1' && eded <= '7') {
 
 //         if (eded == '1') {
@@ -54,17 +61,14 @@
 //             alert('Yeddinci gun')
 //         }
 
-//     } 
-//     else {
+//     } else {
 //         alert('sehv eded')
 //     }
 // }
 
 // gunler(eded)
 
-// let gunler=['mon', 'tue', 'wen', 'thu', 'fri','sut', 'sun' ]
-
-//6. Create a function that takes an array containing only numbers and return the first element.
+//5. Create a function that takes an array containing only numbers and return the first element.
 
 // const array = [5, 10, 18, 56, 15]
 
@@ -74,7 +78,7 @@
 
 // firstNumber()
 
-//7. Given two arguments, return an array which contains these two arguments.
+//6. Given two arguments, return an array which contains these two arguments.
 
 // Examples
 // makePair(1, 2) ➞ [1, 2]
@@ -94,58 +98,3 @@
 //     const arr = [];
 //     return arr.concat(num1, num2)
 // }
-
-
-
-// Место для первой задачи
-// function firstTask() {
-//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-//     const arr = [3, 5, 8, 16, 20, 23, 50];
-//     const result = [];
-//     for (let i=0;i<arr.length;i++){
-//         result[i]=arr[i]
-//     }
-
-//     // Пишем решение вот тут
-    
-    
-//     // Не трогаем
-//     return result;
-// }
-// console.log(firstTask())
-
-// function secondTask() {
-//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-//     const data = [5, 10, 'Shopping', 20, 'Homework'];
-//     for(let i=0;i<data.length;i++){
-//         if(typeof(data[i])=="number"){
-//             data[i]*=2
-//         }else{
-//             data[i]+=" - done"
-//         }
-//     }
-
-//     // Пишем решение вот тут
-    
-    
-//     // Не трогаем
-//     console.log(data)
-// }
-// secondTask()
-
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-    // Пишем решение вот тут
-   
-    for (let i = 1; i <= data.length; i++) {
-        result[i - 1] = data[data.length - i]
-    }
-    
-    
-    // Не трогаем
-    console.log(result);
-}
-thirdTask()
