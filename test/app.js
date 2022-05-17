0b8991a67e7b424786bb3e42bef48d6d24aa9364
AlimaKarimova <way.s@inbox.ru>
2022-05-17 19:31:30 +0400

    quiz app

--- test/app.js
+++ test/app.js
@@ -1,10 +1,78 @@
-// document.querySelector('.login-form form').reset();
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
 
-const userName = document.getElementById('userName');
+    let quizDB = {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['js', 'script', 'scripting', 'javascript'],
+        correctAnswer: 2
 
-function validateUserName() {
-	if (userName.value == '' || userName.value.indexOf('@') == -1) {
-		userName.innerHTML = 'salam'
-	}
-}
-userName.addEventListener('keyup', validateUserName)
+    };
+
+    const question = document.getElementById('addQuestion');
+    const box = document.querySelector('.quizes-box');
+
+    question.addEventListener('click', createNewQuiz);
+
+
+
+    function createNewQuiz() {
+        let list = document.createElement('div');
+        list.classList.add('.quiz-box')
+        list.innerHTML += `
+        <div class="quiz-title"><h3 class="quiz-text">${quizDB.question}</h3>
+        <span class="bin" ><i class="fa-solid fa-circle-minus"></i></span></div>
+        <ol>   
+        <li>${quizDB.options[0]}</li>
+        <li>dfdf</li>
+        <li>dsfdf</li>
+        <li>vdfsfd</li>
+        </ol>`
+
+        let variants = list.querySelectorAll('ol li');
+        variants.forEach(function (elem, index) {
+            elem.textContent = quizDB.options[index];
+
+        })
+
+        box.append(list);
+        document.querySelectorAll('.bin i').forEach((item, i) => {
+            item.addEventListener('click', (e) => {
+                list.remove();
+            })
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
