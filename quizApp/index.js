70960ffabb2f13b99b1cf41f27898a74e1101aac
AlimaKarimova <way.s@inbox.ru>
2022-05-20 22:47:27 +0400

    .

--- quizApp/index.js
+++ quizApp/index.js
@@ -1,34 +1,40 @@
-'use strict';
-
-const old = JSON.parse(localStorage.getItem('DataBase'));
-const DB = JSON.parse(JSON.stringify(old));
-const quizBox = document.querySelector('.quiz-box');
-let suallar;
-
+const startBox=document.querySelector('.start-box');
+const mainBox =document.querySelector('.main-box');
+mainBox.classList.add('hide')
+const QuestionsDB = [{
+        text: "İlde neçə ay var?",
+        options: ['5', '10', '15', '12'],
+        correctAnswer: '12'
+    },
+    {
+        text: "Nece qite var?",
+        options: ['5', '6', '4', '10'],
+        correctAnswer: '4'
+    }
+];
 
 function addQuestion() {
-    quizBox.innerHTML = "";
-    for (let i in DB) {
+    for (let i in QuestionsDB) {
+        const quizBox = document.createElement('div');
+        quizBox.classList.add('quiz-box', 'container')
+        quizBox.innerHTML = "";
         quizBox.innerHTML += ` 
-        <div> 
-        <h4>${DB[i].text}</h4>
+        <h4>${QuestionsDB[i].text}</h4>
         <ul>
-        <li>${DB[i].options[0]}</li>
-        <li>${DB[i].options[1]}</li>
-        <li>${DB[i].options[2]}</li>
-        <li>${DB[i].options[3]}</li>
-        </div>  
+        <li><input type="radio" name="" id="">${QuestionsDB[i].options[0]}</li>
+        <li><input type="radio" name="" id="">${QuestionsDB[i].options[1]}</li>
+        <li><input type="radio" name="" id="">${QuestionsDB[i].options[2]}</li>
+        <li><input type="radio" name="" id="">${QuestionsDB[i].options[3]}</li>
+        <button type="button" class="btn" >Back</button>
+        <button type="button" class="btn" >Next</button>
     </ul>`
+
+       mainBox.append(quizBox);
+       
     };
-
-    suallar = document.querySelectorAll('.quiz-box ul li');
-    suallar.forEach(function (sual) {
-        sual.classList.add('none-checked')
-        sual.addEventListener('click', (e) => {
-            e.target.classList.remove('none-checked')
-            e.target.classList.add('checked');
-        })
-    })
-
 };
 addQuestion();
+function startQuiz(){
+    startBox.classList.add('hide');
+    mainBox.classList.remove('hide');
+}
